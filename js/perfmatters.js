a0c82026c21a318b821a45629e0a9b4646c9ee61
AlyeB <AlyeB@ALYEs-MBP.home>
2017-02-12 19:53:55 -0500

    Add google analytics script to file

--- js/perfmatters.js
+++ js/perfmatters.js
@@ -12,3 +12,13 @@ function logCRP() {
 window.addEventListener("load", function(event) {
   logCRP();
 });
+
+//deffered javascript
+(function(w,g){w['GoogleAnalyticsObject']=g;
+    w[g]=w[g]||function(){(w[g].q=w[g].q||[]).push(arguments)};w[g].l=1*new Date();})(window,'ga');
+
+    // Optional TODO: replace with your Google Analytics profile ID.
+    ga('create', 'UA-XXXX-Y');
+    ga('send', 'pageview');
+
+<script src="http://www.google-analytics.com/analytics.js">
